0f6df201f69b94c359c7523b07e931862a2fde23
jrvldam <jrvldam@gmail.com>
2023-06-04 13:38:30 +0200

    code format

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -39,7 +39,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 			app.serverError(w, err)
 		}
 
-    return
+		return
 	}
 
 	data := app.newTemplateData(r)
@@ -63,5 +63,5 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
