9ce80c97a9d8b7bd04a4c2120e6a7078311ca418
jrvldam <jrvldam@gmail.com>
2023-06-20 00:55:07 +0200

    end to end testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.jrvldam.github.com/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	app := newTestApplicaton(t)
 
-	ping(rr, r)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	rs := rr.Result()
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
